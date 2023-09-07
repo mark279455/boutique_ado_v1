528314766017df82c7f0de802c0eaf704919d5ca
mark279455 <mark279455@ntlworld.com>
2023-09-07 13:00:34 +0100

    added product views and templates

--- boutique_ado/products/views.py
+++ boutique_ado/products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+
+def all_products(request):
+    """returns all products - sorting and search"""
+
+    products = Product.objects.all()
+
+    context = {
+        "products": products,
+    }
+    return render(request, "products/products.html", context)
