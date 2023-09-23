8388454513938c0f6d16eb25569e34dae2d0f205
mark279455 <mark279455@ntlworld.com>
2023-09-23 17:00:58 +0100

    Added checkout success logic

--- boutique_ado/checkout/urls.py
+++ boutique_ado/checkout/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.checkoutView, name="checkoutView")
+    path("", views.checkoutView, name="checkoutView"),
+    path(
+        "checkout_success/<order_number>",
+        views.checkout_success,
+        name="checkout_success",
+    ),
 ]
