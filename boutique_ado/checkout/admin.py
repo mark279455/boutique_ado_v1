1c3df203c2d6ec8ee3d720b3e89a44fd86da8aaa
mark279455 <mark279455@ntlworld.com>
2023-09-25 16:49:15 +0100

    Comleted webhook handler

--- boutique_ado/checkout/admin.py
+++ boutique_ado/checkout/admin.py
@@ -16,6 +16,8 @@ class OrderAdmin(admin.ModelAdmin):
         "delivery_cost",
         "order_total",
         "grand_total",
+        "original_bag",
+        "stripe_pid",
     )
     fields = (
         "order_number",
@@ -32,6 +34,8 @@ class OrderAdmin(admin.ModelAdmin):
         "delivery_cost",
         "order_total",
         "grand_total",
+        "original_bag",
+        "stripe_pid",
     )
     list_display = (
         "order_number",
