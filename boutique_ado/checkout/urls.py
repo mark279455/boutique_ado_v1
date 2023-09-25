ab8313c34f5441ae8655c245da97190584e5e4d3
mark279455 <mark279455@ntlworld.com>
2023-09-25 14:49:35 +0100

    Added class methods to webhook handler and webhook view

--- boutique_ado/checkout/urls.py
+++ boutique_ado/checkout/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path("", views.checkoutView, name="checkoutView"),
@@ -7,5 +8,10 @@ urlpatterns = [
         "checkout_success/<order_number>",
         views.checkout_success,
         name="checkout_success",
+    ),
+    path(
+        "wh/",
+        webhook,
+        name="webhook",
     ),
 ]
