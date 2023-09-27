40ab7efa5ca4eb4f084ec3c7f1f4d4559b93cb24
mark279455 <mark279455@ntlworld.com>
2023-09-27 18:22:58 +0100

    profiles part 6 - completed confirmation emails

--- boutique_ado/checkout/views.py
+++ boutique_ado/checkout/views.py
@@ -6,8 +6,8 @@ from django.conf import settings
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
-from profiles.forms import UserProfileForm
 from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from bag.contexts import bag_contents
 
 import stripe
@@ -92,6 +92,7 @@ def checkoutView(request):
                     order.delete()
                     return redirect(reverse("view_bag"))
 
+            # Save the info to the user's profile if all is well
             request.session["save_info"] = "save-info" in request.POST
             return redirect(reverse("checkout_success", args=[order.order_number]))
         else:
@@ -104,7 +105,7 @@ def checkoutView(request):
     else:
         bag = request.session.get("bag", {})
         if not bag:
-            messages.error(request, "Your bag is empty")
+            messages.error(request, "There's nothing in your bag at the moment")
             return redirect(reverse("products"))
 
         current_bag = bag_contents(request)
@@ -116,6 +117,7 @@ def checkoutView(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
+        # Attempt to prefill the form with any info the user maintains in their profile
         if request.user.is_authenticated:
             try:
                 profile = UserProfile.objects.get(user=request.user)
@@ -139,7 +141,9 @@ def checkoutView(request):
 
     if not stripe_public_key:
         messages.warning(
-            request, "Stripe public key is missing. Did you forget to set it?"
+            request,
+            "Stripe public key is missing. \
+            Did you forget to set it in your environment?",
         )
 
     template = "checkout/checkout.html"
@@ -153,29 +157,32 @@ def checkoutView(request):
 
 
 def checkout_success(request, order_number):
-    """Handle successful checkouts"""
+    """
+    Handle successful checkouts
+    """
     save_info = request.session.get("save_info")
     order = get_object_or_404(Order, order_number=order_number)
 
-    profile = UserProfile.objects.get(user=request.user)
-    # Attach the user's profile to the order
-    order.user_profile = profile
-    order.save()
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
 
-    # Save ther user's info
-    if save_info:
-        profile_data = {
-            "default_phone_number": order.phone_number,
-            "default_country": order.country,
-            "default_postcode": order.postcode,
-            "default_town_or_city": order.town_or_city,
-            "default_street_address1": order.street_address1,
-            "default_street_address2": order.street_address2,
-            "default_county": order.county,
-        }
-        user_profile_form = UserProfileForm(profile_data, instance=profile)
-        if user_profile_form.is_valid():
-            user_profile_form.save()
+        # Save ther user's info
+        if save_info:
+            profile_data = {
+                "default_phone_number": order.phone_number,
+                "default_country": order.country,
+                "default_postcode": order.postcode,
+                "default_town_or_city": order.town_or_city,
+                "default_street_address1": order.street_address1,
+                "default_street_address2": order.street_address2,
+                "default_county": order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
 
     messages.success(
         request,
